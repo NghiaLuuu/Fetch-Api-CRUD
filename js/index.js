ca474a972b24eefe4a93a232ad1108771fb38788
Nghia <luutrungnghia19012003>
2023-05-09 07:58:49 +0700

    3

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
-var fetApi = 'https://64598c608badff578e0fbc61.mockapi.io/apitable'
-fetch(fetApi)
+var Api = 'https://64598c608badff578e0fbc61.mockapi.io/apitable'
+fetch(Api)
     .then(function(response) {
         return response.json();
     })
-    .then(function(post) {
-        console.log(post)
+    .then(function(data) {
+        render(data);
     })
+
+function render(data) {
+    console.log(data);
+    var table = document.getElementsByTagName('table')[0];
+    for (var i = 1, row;
+        (row = table.rows[i]) && i <= data.length; i++) {
+        row.cells[0].innerHTML = `${data[i-1].id}`;
+        row.cells[1].innerHTML = `
+            <img src="${data[i-1].image}"> </img>
+        `;
+        row.cells[2].innerHTML = `${data[i-1].name}`;
+
+    }
+
+}
